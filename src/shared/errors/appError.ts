31ef97dbc77cfa7f6245ff367d045cc9a9e44849
Tatiane Weitzel <weitzeltati@gmail.com>
2023-05-25 18:01:44 -0300

    add typeorm

--- src/shared/errors/appError.ts
+++ src/shared/errors/appError.ts
@@ -1,4 +1,12 @@
-class AppErroer {
-  
+class AppError {
+  public readonly message : string;
+  public readonly statusCode : number;
+
+  constructor(message: string, statusCode = 400){
+    this.message = message;
+    this.statusCode = statusCode;
+  }
 
 }
+
+export default AppError;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,35 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { Response, Request, NextFunction } from 'express';
 import cors from 'cors';
 import routes from '@shared/routes';
+import AppError from '@shared/errors/appError';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(routes)
+app.use(routes);
+
+app.use(
+  (
+    error: Error,
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) => {
+    if(error instanceof AppError){
+      return response.status(error.statusCode).json({
+        status:'error',
+        message: error.message
+      })
+    }
+    return response.status(500).json({
+      status:'error',
+      message: 'Internal server error'
+    })
+
+  },
+);
 app.listen(3333, () => {
   console.log('Servidor rodando na porta 3333');
 });
